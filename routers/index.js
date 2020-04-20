d7d7b69ce9a4e77ed60d5dacfe2f2d230cee8d5f
linboxuan <377760330@qq.com>
2020-04-21 02:32:33 +0800

    test

--- routers/index.js
+++ routers/index.js
@@ -1,21 +1,22 @@
 /*
  * @Author: your name
  * @Date: 2020-03-26 00:01:51
- * @LastEditTime: 2020-04-17 03:45:58
+ * @LastEditTime: 2020-04-21 02:31:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /api/Users/linboxuan/vscodeProjects/blog/routers/index.js
  */
-const tagAdminController = require('../controller/tagController');
-const articleAdminConeroller = require('../controller/articleController');
-const userConeroller = require('../controller/userController');
-const articleConeroller = require('../controller/articleController/api')
-const tagController = require('../controller/tagController/api')
+// const tagAdminController = require('../controller/tagController');
+// const articleAdminConeroller = require('../controller/articleController');
+// const userConeroller = require('../controller/userController');
+// const articleConeroller = require('../controller/articleController/api')
+// const tagController = require('../controller/tagController/api')
+const models = require('../models/article')
 
 module.exports = app => {
-    app.use(tagAdminController.routes(), tagAdminController.allowedMethods())
-    app.use(articleAdminConeroller.routes(), articleAdminConeroller.allowedMethods())
-    app.use(userConeroller.routes(), userConeroller.allowedMethods())
-    app.use(articleConeroller.routes(), articleConeroller.allowedMethods())
-    app.use(tagController.routes(), tagController.allowedMethods())
+    // app.use(tagAdminController.routes(), tagAdminController.allowedMethods())
+    // app.use(articleAdminConeroller.routes(), articleAdminConeroller.allowedMethods())
+    // app.use(userConeroller.routes(), userConeroller.allowedMethods())
+    // app.use(articleConeroller.routes(), articleConeroller.allowedMethods())
+    // app.use(tagController.routes(), tagController.allowedMethods())
 }
