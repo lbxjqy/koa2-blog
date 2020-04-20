ab94fe5a03ce86c925c531f4a872a5d2dc72f5c8
linboxuan <377760330@qq.com>
2020-04-21 02:43:40 +0800

    test

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-26 00:01:51
- * @LastEditTime: 2020-04-21 02:40:31
+ * @LastEditTime: 2020-04-21 02:43:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /api/Users/linboxuan/vscodeProjects/blog/routers/index.js
@@ -10,12 +10,12 @@
 // const articleAdminConeroller = require('../controller/articleController');
 // const userConeroller = require('../controller/userController');
 const articleConeroller = require('../controller/articleController/api')
-const tagController = require('../controller/tagController/api')
+// const tagController = require('../controller/tagController/api')
 
 module.exports = app => {
     // app.use(tagAdminController.routes(), tagAdminController.allowedMethods())
     // app.use(articleAdminConeroller.routes(), articleAdminConeroller.allowedMethods())
     // app.use(userConeroller.routes(), userConeroller.allowedMethods())
     app.use(articleConeroller.routes(), articleConeroller.allowedMethods())
-    app.use(tagController.routes(), tagController.allowedMethods())
+    // app.use(tagController.routes(), tagController.allowedMethods())
 }
