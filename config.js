dfb9d63e7463464c618ba9ac1d67d7adff1119c6
linboxuan <377760330@qq.com>
2020-07-31 17:10:20 +0800

    commit

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ if(platform == "DEV") {
 }
 
 if(platform == "PROD") {
-    config.mongo.master = 'mongodb://root:123456@39.97.208.148:27017/blog?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+    config.mongo.master = 'mongodb://root:123456@127.0.0.1:27017/blog?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 }
 
 module.exports = config;
